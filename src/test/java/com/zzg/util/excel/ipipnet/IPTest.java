df5ca2404c6874af1062f1921f3acb581a3565ce
zhangzhiguang <zhangzhiguang@192.168.203.142>
2016-03-14 11:05:41 +0800

    新加 ipipnet test类

---
+++ src/test/java/com/zzg/util/excel/ipipnet/IPTest.java
@@ -0,0 +1,35 @@
+package com.zzg.util.excel.ipipnet;
+
+import java.util.Random;
+
+import com.zzg.util.ipipnet.IPUtil;
+
+public class IPTest {
+	public static void main(String[] args) {
+
+		Long st = System.nanoTime();
+		for (int i = 0; i < 1000000; i++) {
+			IPUtil.parseIP(randomIp());
+		}
+		Long et = System.nanoTime();
+		System.out.println((et - st) / 1000 / 1000);
+
+		System.out.println(IPUtil.parseIP("-1001.223.223.1").toString());
+
+	}
+
+	public static String randomIp() {
+		Random r = new Random();
+		StringBuffer str = new StringBuffer();
+		str.append(r.nextInt(1000000) % 255);
+		str.append(".");
+		str.append(r.nextInt(1000000) % 255);
+		str.append(".");
+		str.append(r.nextInt(1000000) % 255);
+		str.append(".");
+		str.append(0);
+
+		return str.toString();
+	}
+
+}
