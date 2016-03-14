879d034cfbf04072475564972a1dd8b2c0e1aa5e
zhangzhiguang <zhangzhiguang@192.168.203.142>
2016-03-14 13:29:18 +0800

    update

--- src/test/java/com/zzg/util/ipipnet/IPTest.java
+++ src/test/java/com/zzg/util/ipipnet/IPTest.java
@@ -2,17 +2,15 @@ package com.zzg.util.ipipnet;
 
 import java.util.Random;
 
-import com.zzg.util.ipipnet.IPUtil;
-
 public class IPTest {
 	public static void main(String[] args) {
 
-		Long st = System.nanoTime();
+		Long st = System.currentTimeMillis();
 		for (int i = 0; i < 1000000; i++) {
 			IPUtil.parseIP(randomIp());
 		}
-		Long et = System.nanoTime();
-		System.out.println((et - st) / 1000 / 1000);
+		Long et = System.currentTimeMillis();
+		System.out.println((et - st));
 
 		System.out.println(IPUtil.parseIP("-1001.223.223.1").toString());
 
